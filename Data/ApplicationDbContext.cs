382bebcfa1217bf9034fb8e0a2ce30ab046ef4e0
shalabh2016 <shalabh2016@gmail.com>
2020-04-16 22:37:07 +0530

    FirstCommit With User Model.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsUsers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=jsusers.db");
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsUsers.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Url { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt {get;set;}
+        public DateTime UpdatedAt { get; set; }
+    }
+}
